ef9c82089d7b31bd0cee88c5dd10ac05bf62cb3b
ZBoyukliev <zarkoboyukliev@gmail.com>
2023-01-16 08:07:31 +0000

    Show games in catalog page

--- src/App.js
+++ src/App.js
@@ -6,19 +6,29 @@ import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import CreateGame from './components/CreateGame/CreateGame';
 import Catalog from './components/Catalog/Catalog';
+import { useEffect, useState } from "react";
+import * as GameService from './services/GameService'
 
 function App() {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+      GameService.getAll()
+      .then(result => {
+          setGames(result);
+      })
+  }, [])
   return (
     <>
     <div id="box">
     <Header/>
       <main id="main-content">
         <Routes>
-          <Route path="/" element={<Home/>}/>
+          <Route path="/" element={<Home games={games}/>}/>
           <Route path="/login" element={<Login/>}/>
           <Route path="/register" element={<Register/>}/>
           <Route path="/create" element={<CreateGame/>}/>
-          <Route path="/catalog" element={<Catalog/>}/>
+          <Route path="/catalog" element={<Catalog games={games}/>}/>
         </Routes>
       </main>
     </div>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,22 +1,14 @@
-import { useEffect, useState } from "react";
-import * as GameService from '../../services/GameService'
+
 import LatestGame from "./LatestGame";
 
-const Home = () => {
-    const [games, setGames] = useState([]);
+const Home = ({games}) => {
 
-    useEffect(() => {
-        GameService.getAll()
-        .then(result => {
-            setGames(result);
-        })
-    }, [])
     return (<section id="welcome-world">
         <div className="welcome-message">
             <h2>ALL new games are</h2>
             <h3>Only in GamesPlay</h3>
         </div>
-        <img src="./images/four_slider_img01.png" alt="hero" />
+        <img src={games.imageUrl} />
         <div id="home-page">
             <h1>Latest Games</h1>
             {games.length > 0 ? 
