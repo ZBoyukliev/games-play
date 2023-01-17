020fc23afe1a1799fe3e716edd4e4a9f0d0093b0
ZBoyukliev <zarkoboyukliev@gmail.com>
2023-01-17 16:18:01 +0000

    Add login

--- src/services/GameService.js
+++ src/services/GameService.js
@@ -1,6 +1,6 @@
+import * as request  from "./requester";
 const baseUrl = 'http://localhost:3030';
 
 export const getAll = () => {
-    return fetch(`${baseUrl}/data/games?sortBy=_createdOn%20desc&distinct=category`)
-    .then(res => res.json())
+    return request.get(`${baseUrl}/data/games?sortBy=_createdOn%20desc&distinct=category`)
 }
